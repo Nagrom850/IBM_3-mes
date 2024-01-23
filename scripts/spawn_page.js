6d3726617939fe646b07468394f740612717e6dc
Nagrom850 <mratgris@gmail.com>
2024-01-23 20:55:55 +0100

    ça avance...

--- scripts/spawn_page.js
+++ scripts/spawn_page.js
@@ -1,3 +1,5 @@
+
+//Tant que la page n'est pas chargée, on cache le body pour faire tout apparaitre en même temps
 window.addEventListener('load', function() {
     document.body.style.opacity = '1';
 });
